0f2980238fae22bb6ebe8960a5a06a67e7458fcc
Markus Peröbner <markus.peroebner@gmail.com>
2016-10-27 10:54:00 +0200

    enables parsing of multi line messages
    Will now parse multi line messages instead of just the last line from the commit message.

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ Gitlog.prototype._write = function(chunk, encoding, callback) {
       } else if (pair[0].toLowerCase() === 'date') {
         this._current.date = new Date(pair[1].trim());
       } else {
-        this._current.message = '\n' + chunk;
+        this._current.message += '\n' + chunk;
         //this._current[pair[0]] = pair[1];
       }
     } else {
